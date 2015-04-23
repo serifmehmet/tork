91c568ed55458a7343565f4e685b732cddddc03d
Mehmet Serif <mehmetserif@gmail.com>
2015-04-23 11:36:56 +0300

    Login redirect eklendi

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,8 @@ module Torkgarage
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.i18n.default_locale = :tr
     
   end
 end

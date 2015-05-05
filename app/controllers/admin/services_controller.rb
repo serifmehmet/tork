9c9df92aa9b5242ad9b64ea97fee1f4a2869d4ea
Mehmet Serif <mehmetserif@gmail.com>
2015-05-06 01:58:33 +0300

    Image upload updated

--- app/controllers/admin/services_controller.rb
+++ app/controllers/admin/services_controller.rb
@@ -16,12 +16,14 @@ class Admin::ServicesController < ApplicationController
     @service = Service.new(service_params)
 
     if @service.save
-      if params[:images]
+      
         
-          @image = @service.images.create!(:image => params[:images][:image], :imageable_id => @service.id, :image_type => params[:images][:image_type])
-          @icon_image = @service.images.create!(:image => params["icon_image"], :imageable_id => @service.id, :image_type => params["icon_image_type"])
-        
-      end
+      @image_title = @service.images.create!(:image => params[:service][:images_title][:image], :imageable_id => @service.id, :image_type => params[:service][:images_title][:image_type])
+      @icon_image = @service.images.create!(:image => params[:service][:images_icon][:image], :imageable_id => @service.id, :image_type => params[:service][:images_icon][:image_type])
+      
+      # if params[:images_icon]
+
+      # end
       
       flash[:notice] = "Hizmet sayfası başarıyla oluşturuldu!"
       redirect_to admin_services_path
@@ -33,6 +35,7 @@ class Admin::ServicesController < ApplicationController
 
   def edit
     @service = Service.find(params[:id])
+    @images = @service.images.build
   end
 
   def update
@@ -62,6 +65,6 @@ class Admin::ServicesController < ApplicationController
 
   	def service_params
       params.require(:service).permit(:service_name, :service_description, :meta_keywords, 
-        images_attributes: [:id, :imageable_id, :image])
+        images_title_attributes: [:id, :imageable_id, :image], images_icon_attributes: [:id, :imageable_id, :image])
     end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,10 @@
 class Service < ActiveRecord::Base
 	has_many :images, as: :imageable, :dependent => :destroy
+	has_many :images_title,-> {where "image_type = 2"}, as: :imageable, class_name: "Image", :dependent => :destroy
+	has_many :images_icon,-> {where "image_type = 1"}, as: :imageable, class_name: "Image", :dependent => :destroy
+	has_many :images_regular,-> {where "image_type = 3"}, as: :imageable, class_name: "Image", :dependent => :destroy
+
 	accepts_nested_attributes_for :images
+
 	
 end
